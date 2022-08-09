48852338395b350a1649b99cf22dcd3f83262afe
root <root@CG352.CG.LOCAL>
2022-08-09 07:51:46 -0300

    chore: aprimoradas configurações e mapeamentos de pastas

--- __init__.py
+++ __init__.py
@@ -2,9 +2,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
-
 incomes = [
   { 'description': 'salary', 'amount': 5000 }
 ]
