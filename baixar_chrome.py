a914dd6fceb89402bcfebdfeb7e229072d865b13
Claudio Alessandro Nascimento Santos <claudioalessandro_22@hotmail.com>
2022-08-16 09:57:33 -0300

    Add files via upload
    funcoes de baixar chrome e apagar arquivos dentro do airflow

--- baixar_chrome.py
+++ baixar_chrome.py
@@ -1,9 +1,15 @@
 import requests
 from zipfile import ZipFile
 import os
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 
 #baixar driver chrome mais atualizado
-def baixar_chrome(url, endereco):
+def baixar_chrome():
+    url = 'https://chromedriver.storage.googleapis.com/103.0.5060.134/chromedriver_win32.zip'
+    endereco = 'chromedriver.zip'
     resposta = requests.get(url)
     with open(endereco,'wb') as novo_arquivo:
         novo_arquivo.write(resposta.content)
@@ -11,13 +17,23 @@ def baixar_chrome(url, endereco):
     z.extractall()
     z.close
 
-
-
 #exclui arquivos utilizados
 def exclui_arquivos():
     os.remove("chromedriver.zip")
     os.remove("chromedriver.exe")
-    
-if __name__ == "__main__":
-    baixar_chrome('https://chromedriver.storage.googleapis.com/103.0.5060.134/chromedriver_win32.zip', 'chromedriver.zip')
-    exclui_arquivos()
+   
+
+with DAG('baixar_chrome', start_date=datetime(2022,8,1),schedule_interval='30 * * * *',catchup=False) as dag:
+
+    baixar_chrome = PythonOperator(
+        task_id = 'baixar_chrome',
+        python_callable = baixar_chrome
+    )
+
+    exclui_arquivos = PythonOperator(
+        task_id = 'exclui_arquivos',
+        python_callable = exclui_arquivos
+    )
+
+
+baixar_chrome >> exclui_arquivos
